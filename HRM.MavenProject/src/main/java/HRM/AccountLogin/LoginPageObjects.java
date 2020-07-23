44c8575b7f685855042c46b66b75105577e481cb
palle <palle@DESKTOP-R2HLTN2.attlocal.net>
2020-07-22 23:10:01 -0400

    First commit

---
+++ HRM.MavenProject/src/main/java/HRM/AccountLogin/LoginPageObjects.java
@@ -0,0 +1,13 @@
+package HRM.AccountLogin;
+
+import org.testng.annotations.Test;
+
+public class LoginPageObjects 
+{
+  
+	@Test
+	public void login()
+	{
+		System.out.println("My framework Configuration message");
+	}
+}
---
+++ HRM.MavenProject/src/test/java/HRM/AccountLogin/Verifylogin.java
@@ -0,0 +1,25 @@
+package HRM.AccountLogin;
+
+
+
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.Test;
+
+
+
+
+public class Verifylogin 
+{
+   
+    @Test
+    public void simpletest()
+    {
+        System.out.println("My Maven Project");
+    }
+    
+    @AfterTest
+    public void statement()
+{
+	System.out.println("After test executed");
+}
+}
